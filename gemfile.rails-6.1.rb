d43a74aa5f7ace77b50e88826710428ecb0d1098
Anton Ilin <anton@ilin.dn.ua>
2023-10-30 14:12:17 -0700

    Refactor test matrix
    Remove version of rails and ruby from test matrix that reached end of life.

--- gemfile.rails-6.1.rb
+++ gemfile.rails-6.1.rb
@@ -6,4 +6,4 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 # Specify your gem's dependencies in yabeda-activejob.gemspec.
 gemspec
 
-gem "rails", "~> #{ENV['RAILS_VERSION'] || '6.1'}"
+gem "rails", "~> 6.1.0"
